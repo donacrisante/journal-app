f8aa7224839f0984fdab11813350eaef92897989
Donatella Crisante <dona.crisante@zoho.com>
2023-05-28 21:43:27 +0200

    input styling

--- src/components/EntryForm/EntryForm.jsx
+++ src/components/EntryForm/EntryForm.jsx
@@ -7,7 +7,7 @@ export default function EntryForm() {
             <form>
                 <h2 className="form_title">NEW ENTRY</h2>
                 <label htmlFor="motto">Motto</label>
-                <textarea id="motto" placeholder="Enter your text here..."></textarea>
+                <input id="motto" placeholder="Enter your text here..."></input>
                 <label htmlFor="notes">Notes</label>
                 <textarea id="notes" placeholder="Enter your text here..."></textarea>
                 <button className="submit__button" type="submit">Create</button>            
