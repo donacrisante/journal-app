e27875d26f1e9c0b2988bc49a9b19f9b40234353
Donatella Crisante <dona.crisante@zoho.com>
2023-05-29 11:35:38 +0200

    correct tab mistake

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -4,7 +4,7 @@ export default function Tabs() {
     return (
         <>
             <div className="tab-bar">
-                <p className="entries_tab">All entries <span className="entries_badge">3</span></p>
+                <p className="entries_tab">All Entries <span className="entries_badge">3</span></p>
                 <p className="favourites_tab">Favourites <span className="favourites_badge">1</span></p>
             </div>
         </>
